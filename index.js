954f4f07816a11c1f02b509c867659977d4a4bdc
Ash Ryan Arnwine <accounts@ashryan.io>
2022-07-04 02:14:00 +0000

    Return date for command line arg or previous Friday.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("lolz");
+import { previousFriday, parseISO, set } from "date-fns";
+
+const cmdLnArgOrPrevFri = process.argv[2]
+  ? parseISO(process.argv[2])
+  : set(previousFriday(new Date()), {hours: 0, minutes: 0, seconds: 0, milliseconds: 0});
+
+console.log(cmdLnArgOrPrevFri);
